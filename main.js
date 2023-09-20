5105ac17c040f4b2fbb7aae327c8bd2a45b16fc6
Elson Felipe de Oliveira Urbano <elson_felipe@hotmail.com>
2023-09-20 10:39:41 -0300

    realizada a refatoracao e desafios

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
+// variaveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-
-const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-// funcao callback
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', resetWithEnter)
+
+// funcoes callback
 function handleTryClick(event) {
   event.preventDefault() // nao faca o padrao
 
   const inputNumber = document.querySelector("#inputNumber")
 
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
+  if (inputNumber.value == "" || inputNumber.value < 0 || inputNumber.value > 10) {
+    alert("Digite um numero de 0 a 10!")
     
-    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
+  } else {
+    if (Number(inputNumber.value) == randomNumber) {
+      toggleScreen()
+      screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
+    }
+    
+    inputNumber.value = ""
+    xAttempts++
   }
-  
-  inputNumber.value = ""
-  xAttempts++
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+function handleResetClick() {
+  toggleScreen()
   xAttempts = 1
-})
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+function resetWithEnter(e) {
+    if(e.key == 'Enter'&& screen1.classList.contains('hide')) {
+      handleResetClick()
+    }
+}
